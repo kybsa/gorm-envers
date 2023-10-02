5ff29dfd2ec0a65a6d5987d21bbadf9f2712b773
Alejandro Vivas <alejo.vivas@gmail.com>
2023-10-01 20:02:37 -0500

    simple example

---
+++ examples/example1/example1.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/kybsa/gorm-envers/envers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type A struct {
+	ID   uint
+	Name string
+}
+
+func (A) IsAudit() bool {
+	return true
+}
+
+type AsAud struct {
+	A
+	envers.AudData
+}
+
+func (AsAud) TableName() string {
+	return "as_aud"
+}
+
+func main() {
+	gormEnversPlugin := envers.NewGormEnversPlugin(envers.NewConfig())
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{
+		Plugins: map[string]gorm.Plugin{gormEnversPlugin.Name(): gormEnversPlugin},
+	})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+	e := envers.Revinfo{}
+	e.TableName()
+	db.AutoMigrate(&A{}, &envers.Revinfo{}, &AsAud{})
+
+	a := &A{Name: "A-1"}
+	db.Create(a)
+	a.Name = "A-2"
+	db.Save(a)
+	db.Delete(a)
+}
